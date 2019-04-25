f221a67dd45a030159cad42682ab2d526a0de3b5
ashleychari1234 <ashley.c.francisco.21@dartmouth.edu>
2019-04-24 22:41:42 -0400

    workshop files

---
+++ content.js
@@ -0,0 +1,9 @@
+alert("Hello from your Chrome extension!");
+
+var div = document.createElement("div");
+var imgPath = chrome.extension.getURL('img/mohawk.jpg');
+div.innerHTML = `
+      <div id="clippy"></div>
+          <img id="clippyImg" src=${imgPath} />
+      `;
+        document.body.appendChild(div);
